93f798acb2c3f1b5e4e34653b2e62bd3b4537e2b
Sorin Vatasoiu <sorin.vatasoiu@gmail.com>
2015-11-23 00:12:34 -0500

    testing

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
-function load_html(elt, path){
-	elt.innerHTML='<object type="text/html" data="'+path+'" ></object>';
-}
+
+var Player = React.createClass({
+	render: function () {
+		return (
+			<div className="container">
+			  <div className="card">
+			    <div data-path={this.props.front} className='player face front'></div>
+			    <div data-path={this.props.back} className="player face back">
+					  <p>{this.props.name}</p>
+			    </div>
+			  </div>
+			</div>
+		);
+	}
+});
+
+var players = {
+	"Neymar": {name: "Neymar", front: "test.html", back: "test.html"}, 
+	"Iniesta": {front: "test.html", back: "test.html"}
+};
+
+React.render(<Player player={players["Neymar"]} />, document.body, 
+	function () {
+		$(".player").css( "border", "3px solid red" );
+		/*
+		$(".player").each(function(index) {
+			$(this).load($(this).attr("data-path"));
+		});
+		*/
+	}				
+);
